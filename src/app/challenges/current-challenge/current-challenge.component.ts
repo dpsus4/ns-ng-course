7cf1dcb2674852895f38d8d2764d7a6ee980f13e
dpsus4 <fradelpo@yahoo.com>
2020-12-13 19:42:40 +0100

    First styling

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
     selector: 'ns-current-challenge',
     templateUrl: './current-challenge.component.html',
-    styleUrls: ['./current-challenge.component.css']
+    styleUrls: ['./current-challenge.component.css'],
+    moduleId: module.id
 })
 export class CurrentChallengeComponent {
+    challengeDescription = "";
+    currentChallenge = "";
 
+    onSetChallenge() {
+        this.currentChallenge = this.challengeDescription;
+    }
 }
