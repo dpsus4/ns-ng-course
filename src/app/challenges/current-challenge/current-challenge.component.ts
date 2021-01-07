9da2b7d9840680fcd2b300b35b7e8dbc4c1632ab
dpsus4 <fradelpo@yahoo.com>
2021-01-07 22:33:03 +0100

    Added actionbars

--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,7 +1,11 @@
 import { Input } from "@angular/core";
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
+import { RouterExtensions } from "@nativescript/angular";
+import { isAndroid, Page } from "@nativescript/core";
 import { ItemEventData } from "@nativescript/core/ui/list-view";
+
+declare var android: any;
 
 @Component({
     selector: "ns-current-challenge",
@@ -15,9 +19,20 @@ export class CurrentChallengeComponent {
     // onItemTap(args: ItemEventData) {
     //     console.log(args);
     // }
-    constructor(private router: Router) { }
+    constructor(private router: RouterExtensions, private page: Page) { }
 
     onEdit() {
         this.router.navigate(["/edit-challenge"]);
+    }
+
+    onLoadedActionBar() {
+        if (isAndroid) {
+            const androidToolbar = this.page.actionBar.nativeView;
+            const backButton = androidToolbar.getNavigationIcon();
+
+            if (backButton) {
+                backButton.setColorFilter(android.graphics.Color.parseColor('#171717'), (<any>android.graphics).PorterDuff.Mode.SRC_ATOP);
+            }
+        }
     }
 }
