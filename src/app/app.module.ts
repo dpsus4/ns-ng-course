684a1e12a3a81f7cb32bf5f29a94490a33af15b2
dpsus4 <fradelpo@yahoo.com>
2021-01-20 19:43:13 +0100

    Added modal dialog

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,5 @@
 import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptFormsModule, NativeScriptModule } from "@nativescript/angular";
+import { ModalDialogService, NativeScriptFormsModule, NativeScriptModule } from "@nativescript/angular";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -12,6 +12,7 @@ import { AuthComponent } from "./auth/auth.component";
 import { TodayComponent } from "./challenges/today/today.component";
 import { ActionBarComponent } from "./shared/ui/action-bar/action-bar.component";
 import {NativeScriptUISideDrawerModule} from 'nativescript-ui-sidedrawer/angular/nativescript-ui-sidedrawer-angular';
+import { DayModalComponent } from "./challenges/day-modal/day-modal.component";
 
 @NgModule({
     bootstrap: [
@@ -32,11 +33,13 @@ import {NativeScriptUISideDrawerModule} from 'nativescript-ui-sidedrawer/angular
         AbsoluteComponent,
         AuthComponent,
         TodayComponent,
-        ActionBarComponent
+        ActionBarComponent,
+        DayModalComponent
     ],
     providers: [],
     schemas: [
         NO_ERRORS_SCHEMA
-    ]
+    ],
+    entryComponents: [DayModalComponent]
 })
 export class AppModule { }
--- src/app/challenges/current-challenge/current-challenge.component.ts
+++ src/app/challenges/current-challenge/current-challenge.component.ts
@@ -1,9 +1,11 @@
-import { Input } from "@angular/core";
+import { Input, ViewContainerRef } from "@angular/core";
 import { Component } from "@angular/core";
 import { Router } from "@angular/router";
 import { RouterExtensions } from "@nativescript/angular";
 import { isAndroid, Page } from "@nativescript/core";
 import { ItemEventData } from "@nativescript/core/ui/list-view";
+import { ModalDialogService } from "@nativescript/angular";
+import { DayModalComponent } from "../day-modal/day-modal.component";
 
 @Component({
     selector: "ns-current-challenge",
@@ -17,11 +19,22 @@ export class CurrentChallengeComponent {
     // onItemTap(args: ItemEventData) {
     //     console.log(args);
     // }
-    constructor(private router: RouterExtensions) {}
+    constructor(
+        private router: RouterExtensions,
+        private modalDialog: ModalDialogService,
+        private vcRef: ViewContainerRef
+    ) {}
 
     onEdit() {
         this.router.navigate(["/challenges/edit"], {
             transition: { name: "slideLeft" },
+        });
+    }
+
+    onChangeStatus() {
+        this.modalDialog.showModal(DayModalComponent, {
+            fullscreen: true,
+            viewContainerRef: this.vcRef,
         });
     }
 }
