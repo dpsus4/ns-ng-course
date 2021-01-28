369a5f164b2f69eed9c37c9dc9e0fc270b4080ae
dpsus4 <fradelpo@yahoo.com>
2021-01-28 22:37:55 +0100

    Added CSS

--- src/app/challenges/today/today.component.ts
+++ src/app/challenges/today/today.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'ns-today',
-  templateUrl: './today.component.html',
-  styleUrls: ['./today.component.css'],
-  moduleId: module.id
+    selector: "ns-today",
+    templateUrl: "./today.component.html",
+    styleUrls: ["./today.component.css"],
+    moduleId: module.id,
 })
 export class TodayComponent implements OnInit {
+    isHighlighted = false;
 
-  constructor() { }
+    constructor() {}
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {}
 
+    // onDemo() {
+    //     this.isHighlighted = !this.isHighlighted;
+    // }
 }
