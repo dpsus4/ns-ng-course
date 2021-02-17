d435081f5d1c2f3e8c4c23d2134b20b0f1070300
dpsus4 <fradelpo@yahoo.com>
2021-02-17 20:38:19 +0100

    Adding day state

--- src/app/challenges/challenge-actions/challenge-actions.module.ts
+++ src/app/challenges/challenge-actions/challenge-actions.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { ChallengeActionsComponent } from "./challenge-actions.component";
 import { NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptCommonModule } from "@nativescript/angular";
 
 @NgModule({
     declarations: [ChallengeActionsComponent],
+    imports: [NativeScriptCommonModule],
     exports: [ChallengeActionsComponent],
     schemas: [NO_ERRORS_SCHEMA]
 })
