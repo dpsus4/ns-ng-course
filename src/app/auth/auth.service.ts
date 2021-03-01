a9cd44d87792663c97f2399a143d167cff0118a6
dpsus4 <fradelpo@yahoo.com>
2021-03-01 20:44:09 +0100

    Added user handling

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -2,16 +2,35 @@ import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
 import { throwError } from "rxjs";
 import { catchError } from "rxjs/operators";
+import { User } from "./user.model";
+import { take, tap } from "rxjs/operators";
+import { BehaviorSubject } from "rxjs";
 
 const FIREBASE_API_KEY = "";
 
+interface AuthResponseData {
+    kind: string;
+    idToken: string;
+    email: string;
+    refreshToken: string;
+    expiresIn: string;
+    localId: string;
+    registered?: boolean;
+}
+
 @Injectable({ providedIn: "root" })
 export class AuthService {
+    private _user = new BehaviorSubject<User>(null);
+
     constructor(private http: HttpClient) {}
+
+    get user() {
+        return this._user.asObservable();
+    }
 
     signUp(email: string, password: string) {
         return this.http
-            .post(``, {
+            .post<AuthResponseData>(``, {
                 email: email,
                 password: password,
                 returnSecureToken: true,
@@ -21,6 +40,16 @@ export class AuthService {
                     this.handleError(errorRes.error.error.message);
 
                     return throwError(errorRes);
+                }),
+                tap((resData) => {
+                    if (resData && resData.idToken) {
+                        this.handleLogin(
+                            email,
+                            resData.idToken,
+                            resData.localId,
+                            parseInt(resData.expiresIn)
+                        );
+                    }
                 })
             );
         // .subscribe((resData) => {
@@ -58,5 +87,20 @@ export class AuthService {
             default:
                 alert("Authentication failed, check your credentials");
         }
+    }
+
+    private handleLogin(
+        email: string,
+        token: string,
+        userId: string,
+        expiresIn: number
+    ) {
+        const expirationDate = new Date(
+            new Date().getTime() + expiresIn * 1000
+        );
+
+        const user = new User(email, userId, token, expirationDate);
+
+        this._user.next(user);
     }
 }
